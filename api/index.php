b2ea0931044ac0e4624e315c38d07a70291178e1
dungnm.dev <mdung.dev@gmail.com>
2024-09-09 21:15:30 +0700

    add config deploy verce

--- api/index.php
+++ api/index.php
@@ -1,13 +1,3 @@
 <?php
 
-// Load the Laravel application
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Run the application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
